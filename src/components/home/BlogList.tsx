1d41c9e83e316be58e05936c56caf3c035613fb5
jishadaly <jishadlm10@gmail.com>
2024-12-22 21:08:39 +0530

    update app logo

--- src/components/home/BlogList.tsx
+++ src/components/home/BlogList.tsx
@@ -3,6 +3,7 @@ import BlogCard from './BlogCard'
 // import { useInView } from 'react-intersection-observer'
 import { getBlogs, removeBlog } from '@/api/servies/userService'
 import { IBlogPost } from '@/types/Blogtype'
+import BlogListSkeleton from './BlogListSkeleton'
 
 
 
@@ -80,24 +81,4 @@ export default function BlogList() {
   )
 }
 
-function BlogListSkeleton() {
-  return (
-    <div className="space-y-6">
-      {[...Array(5)].map((_, i) => (
-        <div key={i} className="border border-gray-200 rounded-lg p-4 flex space-x-4 animate-pulse">
-          <div className="flex-shrink-0 w-24 h-24 bg-gray-200 rounded-lg"></div>
-          <div className="flex-grow space-y-2">
-            <div className="h-6 bg-gray-200 rounded w-3/4"></div>
-            <div className="h-4 bg-gray-200 rounded w-full"></div>
-            <div className="h-4 bg-gray-200 rounded w-full"></div>
-            <div className="flex justify-between">
-              <div className="h-4 bg-gray-200 rounded w-1/4"></div>
-              <div className="h-4 bg-gray-200 rounded w-1/4"></div>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  )
-}
 
