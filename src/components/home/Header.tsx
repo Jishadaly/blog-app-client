ec7a5585fa8eae822b5d288ed2a347febbc4f514
jishadaly <jishadlm10@gmail.com>
2024-12-22 21:17:36 +0530

    added new header desing for home page

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -49,7 +49,7 @@ export default function Header() {
   }
 
   return (
-    <header className="bg-white border-b sticky top-0 z-10">
+    <header className="backdrop-blur border-b sticky top-0 z-10">
       <div className="container mx-auto px-4 py-4 flex items-center justify-between">
         <div className="flex items-center">
           <Sheet>
